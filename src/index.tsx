2e85ab6292dc8f60d8edefd07a6e61611f4b4e24
Jinnan Zhang <vannizhang@gmail.com>
2021-04-13 15:38:53 -0700

    remove redux for now

--- src/index.tsx
+++ src/index.tsx
@@ -2,24 +2,21 @@ import './styles/index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider as ReduxProvider } from 'react-redux';
-
-import configureAppStore, { getPreloadedState } from './store/configureStore';
+// import { Provider as ReduxProvider } from 'react-redux';
+// import configureAppStore, { getPreloadedState } from './store/configureStore';
 
 import AppContextProvider from './contexts/AppContextProvider';
 
 import { RootPage } from './pages';
 
 (async () => {
-    const preloadedState = getPreloadedState();
+    // const preloadedState = getPreloadedState();
 
     ReactDOM.render(
         <React.StrictMode>
-            <ReduxProvider store={configureAppStore(preloadedState)}>
-                <AppContextProvider>
-                    <RootPage />
-                </AppContextProvider>
-            </ReduxProvider>
+            <AppContextProvider>
+                <RootPage />
+            </AppContextProvider>
         </React.StrictMode>,
         document.getElementById('root')
     );

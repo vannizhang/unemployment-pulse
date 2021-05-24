bd28b145139c2bf42bab19798a1102598fadc26a
Jinnan Zhang <vannizhang@gmail.com>
2021-05-24 15:13:46 -0700

    add ranking list

--- src/components/Ranking/Ranking.tsx
+++ src/components/Ranking/Ranking.tsx
@@ -1,11 +1,97 @@
-import React from 'react';
+import React, { useContext, useMemo } from 'react';
 import { Modal } from '../';
 import { Props } from '../Modal/Modal';
+import RankingList from './RankingList';
+
+import { AppContext, AppContextValue } from '../../contexts/AppContextProvider';
+import {
+    UnempolymentData,
+    UnempolymentDataByFIPS,
+} from '../../../shared/types';
+
+export type RankingData = {
+    highest: UnempolymentData[];
+    lowest: UnempolymentData[];
+};
+
+const getRankingData = (
+    data: UnempolymentDataByFIPS,
+    fips: string[],
+    num = 10
+): RankingData => {
+    const sorted = fips
+        .sort((fipsA, fipsB) => {
+            return (
+                data[fipsB].attributes.unemploymentRate -
+                data[fipsA].attributes.unemploymentRate
+            );
+        })
+        .map((fips) => data[fips]);
+
+    const highest = sorted.slice(0, num);
+
+    const lowest = sorted.slice(sorted.length - num, sorted.length).reverse();
+
+    return {
+        highest,
+        lowest,
+    };
+};
 
 const Ranking: React.FC<Props> = ({ isActive, onClose }: Props) => {
+    const { unemploymentDataByFIPS } = useContext<AppContextValue>(AppContext);
+
+    const rankingData4States = useMemo(() => {
+        if (!unemploymentDataByFIPS) {
+            return null;
+        }
+
+        const fipsCodes = Object.keys(unemploymentDataByFIPS);
+
+        const stateFIPS = fipsCodes.filter((fips) => fips.length === 2);
+
+        return getRankingData(unemploymentDataByFIPS, stateFIPS);
+    }, [unemploymentDataByFIPS]);
+
+    const rankingData4Counties = useMemo(() => {
+        if (!unemploymentDataByFIPS) {
+            return null;
+        }
+
+        const fipsCodes = Object.keys(unemploymentDataByFIPS);
+
+        const countyFIPS = fipsCodes.filter((fips) => fips.length === 5);
+
+        return getRankingData(unemploymentDataByFIPS, countyFIPS);
+    }, [unemploymentDataByFIPS]);
+
+    const getContent = () => {
+        if (!rankingData4States || !rankingData4Counties) {
+            return null;
+        }
+
+        return (
+            <div
+                style={{
+                    display: 'flex',
+                }}
+            >
+                <RankingList
+                    rankingData={rankingData4States}
+                    label="50 States and DC"
+                />
+
+                <RankingList
+                    rankingData={rankingData4Counties}
+                    label="3,141 Counties"
+                />
+            </div>
+        );
+    };
+
     return (
         <Modal isActive={isActive} onClose={onClose}>
-            show rankings
+            {getContent()}
         </Modal>
     );
 };
---
+++ src/components/Ranking/RankingList.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { RankingData } from './Ranking';
+import { ThemeText } from '../InfoPanel/InfoText';
+import { UnempolymentData } from '../../../shared/types';
+
+type Props = {
+    rankingData: RankingData;
+    label: string;
+};
+
+const RankingList: React.FC<Props> = ({ rankingData, label }: Props) => {
+    const getList = (data: UnempolymentData[]) => {
+        const list = data.map((d) => {
+            return (
+                <div
+                    key={d.attributes.fips}
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                    }}
+                >
+                    <ThemeText color="blue" size="small">
+                        {d.attributes.name}
+                    </ThemeText>
+
+                    <ThemeText color="blue" size="small">
+                        {d.attributes.unemploymentRate}%
+                    </ThemeText>
+                </div>
+            );
+        });
+
+        return <div>{list}</div>;
+    };
+
+    return rankingData ? (
+        <div
+            style={{
+                margin: '0 1.5rem',
+            }}
+        >
+            <div className="trailer-half">
+                <ThemeText size="medium" color="orange">
+                    {label}
+                </ThemeText>
+            </div>
+
+            <div className="trailer-quarter">
+                <div>
+                    <ThemeText size="small" color="orange">
+                        Highest Unemployment
+                    </ThemeText>
+                </div>
+
+                {getList(rankingData.highest)}
+            </div>
+
+            <div>
+                <div>
+                    <ThemeText size="small" color="orange">
+                        Lowest Unemployment
+                    </ThemeText>
+                </div>
+
+                {getList(rankingData.lowest)}
+            </div>
+        </div>
+    ) : null;
+};
+
+export default RankingList;

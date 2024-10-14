1146c71089eef01707828da5875917bc4f294129
Jinnan Zhang <vannizhang@gmail.com>
2024-10-14 14:59:19 -0700

    chore: remove suppressImplicitAnyIndexErrors from tsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports =  (env, options)=> {
             chunkFilename: '[name].[contenthash].js',
             clean: true
         },
-        devtool: 'source-map',
+        devtool: devMode ? 'source-map' : undefined,
         resolve: {
             extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
         },
